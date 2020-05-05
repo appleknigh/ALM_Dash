98927ff5b814a9d9ca70bbdb46b3c82beb32746a
Chen <cshen28@gmail.com>
2020-05-04 22:19:19 -0400

    reduce wait time

--- app.py
+++ app.py
@@ -21,7 +21,16 @@ from worker import conn
 q = Queue(connection=conn)
 
 #%%
-job_getfit = q.enqueue(utility.getfit, t1='2020-01-01',t2='2020-12-31')
+job_getfit = q.enqueue(utility.getfit, t1='2020-03-01',t2='2020-12-31')
+
+t0 = time.time()
+while job_getfit.result is None:
+    t1 = time.time()
+    t2 = t1-t0
+    time.sleep(5)
+    print('waiting: {}'.format(t2))
+
+print('Finished! Time elapse: {}'.format(t2))
 df_getfit = job_getfit.result
 
 #%%
